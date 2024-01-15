4e6d5f631104b385861d955b684899dd2b1c4349
Grimboide <156683469+Grimboide@users.noreply.github.com>
2024-01-15 15:18:29 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+int x = 5;
+int y = 13;
+int g = 67;
+int r = 13;
+int i = 8;
+
+printf("%d\n", x);
+printf("%d\n", y);
+printf("%d\n", g);
+printf("%d\n", r);
+printf("%d\n", i);
+
+return 0;
+}
