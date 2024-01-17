9111d1808b238400b02d6a4071ea4546fcedb5ee
Grimboide <156683469+Grimboide@users.noreply.github.com>
2024-01-17 09:54:45 +0100

    Add files via upload

--- ex13.c
+++ ex13.c
@@ -15,25 +15,29 @@ scanf("%f", &b);
 printf("ancora un'altro\n");
 scanf("%f", &c);
 
-if(a==b){
-    if(a==c){
-        if(b==c){
+if(a==b && a==c)
+{
 printf("aooooo ma sto triangolo è tutto equilatero\n");
-}}}
-else if(a==b){
-    else if(b!=c){
+}
+else if(a==b)
+{
+    if(b!=c)
+    {
     printf("ma quanto è isoscele questo triangolo papà è buono\n");
-}}
-else if(c==b){
-    else if(b!=a){
+}
+}
+else if(c==b)
+{
+    if(b!=a)
+    {
     printf("ma quanto è isoscele questo triangolo papà è buono\n");
 }}
 else if(a==c){
-    else if(b!=c){
+    if(b!=c){
     printf("ma quanto è isoscele questo triangolo papà è buono\n");
 }}
 else if(a!=b){
-    else if(b!=c){
+    if(b!=c){
     printf("ma sto triangolo è tutto storto :o .... ma è uno scaleno (°o°)\n");
 }}
 return(0);
