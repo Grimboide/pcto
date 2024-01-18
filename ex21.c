af77cbb959d9bf0c1de932a3691e82acc01be3d7
Nevio lo sgrimboidato <156683469+Grimboide@users.noreply.github.com>
2024-01-18 11:33:05 +0000

    a

--- ex21.c
+++ ex21.c
@@ -14,8 +14,12 @@ int main()
     if(lettera==a || lettera==e || lettera==i || lettera==o || lettera==u){
         printf("aoaoao sta lettera è na vocale\n");
     }
-    else{
+    else if(lettera=='b' || lettera=='c' || lettera=='d' || lettera=='f' || lettera=='g' || lettera=='h' || lettera=='l' || lettera=='m' || lettera=='n' || lettera=='p' || lettera=='q' || lettera=='r' || lettera=='s' || lettera=='t' || lettera=='v' || lettera=='z')
+        {
         printf("è tutta consonante sta letterina papà è buono\n");
+    }
+    else{
+        printf("hai inserito un carattere errato\n");
     }
 return 0;
 }
